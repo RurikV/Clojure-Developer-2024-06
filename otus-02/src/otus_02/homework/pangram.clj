f652ed55b80807a0c0b59cca7c85dcc6213671cd
Vladimir Rurik <vladimir.rurik@gmail.com>
2024-07-07 17:57:34 +0300

    feat: Add is-pangram function for checking if a string is a pangram

--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -1,6 +1,13 @@
 (ns otus-02.homework.pangram
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [clojure.set :as set]))
 
+(def alphabet-set (set "abcdefghijklmnopqrstuvwxyz"))
 
-(defn is-pangram [test-string])
+(defn is-pangram [^String input]
+  (let [normalized-chars (-> input
+                             string/lower-case
+                             (string/replace #"[^a-z]" "")
+                             set)]
+    (set/subset? alphabet-set normalized-chars)))
 
