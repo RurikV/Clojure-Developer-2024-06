d904149e416f8e2b9fcde9648f0bcec631122f79
Vladimir Rurik <vladimir.rurik@gmail.com>
2024-07-07 17:40:52 +0300

    feat: Add normalize-string function for input normalization in palindrome.clj

--- otus-02/src/otus_02/homework/palindrome.clj
+++ otus-02/src/otus_02/homework/palindrome.clj
@@ -1,6 +1,13 @@
 (ns otus-02.homework.palindrome
   (:require [clojure.string :as string]))
 
+(def non-alphanumeric-pattern #"\W+")
 
-(defn is-palindrome [test-string])
+(defn normalize-string [^String input]
+  (-> input
+      string/lower-case
+      (string/replace non-alphanumeric-pattern "")))
 
+(defn is-palindrome [^String input]
+  (let [normalized-input (normalize-string input)]
+    (= normalized-input (string/reverse normalized-input))))
