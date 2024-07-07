ee90172b1a8701e9e8302f744738eb00688aeccb
Vladimir Rurik <vladimir.rurik@gmail.com>
2024-07-07 18:39:38 +0300

    feat: Update common_child.clj to complete the assignment

--- otus-02/src/otus_02/homework/common_child.clj
+++ otus-02/src/otus_02/homework/common_child.clj
@@ -1,5 +1,5 @@
-(ns otus-02.homework.common-child)
-
+(ns otus-02.homework.common-child
+  (:require [clojure.string :as str]))
 
 ;; Строка называется потомком другой строки,
 ;; если она может быть образована путем удаления 0 или более символов из другой строки.
@@ -15,8 +15,27 @@
 
 ;; Еще пример HARRY и SALLY. Ответ будет - 2, так как общий элемент у них AY
 
+(defn- create-dp-matrix [rows cols]
+  (vec (repeat (inc rows) (vec (repeat (inc cols) 0)))))
 
-(defn common-child-length [first-string second-string])
+(defn longest-common-subsequence-length [^String first-string ^String second-string]
+  (let [first-length (count first-string)
+        second-length (count second-string)
+        initial-matrix (create-dp-matrix first-length second-length)
+        final-matrix
+        (reduce
+         (fn [matrix [first-idx second-idx]]
+           (assoc-in matrix [first-idx second-idx]
+                     (if (= (.charAt first-string (dec first-idx))
+                            (.charAt second-string (dec second-idx)))
+                       (inc (get-in matrix [(dec first-idx) (dec second-idx)]))
+                       (max (get-in matrix [(dec first-idx) second-idx])
+                            (get-in matrix [first-idx (dec second-idx)])))))
+         initial-matrix
+         (for [first-idx (range 1 (inc first-length))
+               second-idx (range 1 (inc second-length))]
+           [first-idx second-idx]))]
+    (get-in final-matrix [first-length second-length])))
 
-
-
+(defn common-child-length [^String first-string ^String second-string]
+  (longest-common-subsequence-length first-string second-string))
