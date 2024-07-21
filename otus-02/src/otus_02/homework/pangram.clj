8281564e93eab891e9be8cf1d14c4183d4da59c4
Vladimir Rurik <vladimir.rurik@gmail.com>
2024-07-21 20:15:08 +0300

    Refactor is-pangram function for improved performance

--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -1,13 +1,12 @@
-(ns otus-02.homework.pangram
-  (:require [clojure.string :as string]
-            [clojure.set :as set]))
-
-(def alphabet-set (set "abcdefghijklmnopqrstuvwxyz"))
+(ns otus-02.homework.pangram)
 
 (defn is-pangram [^String input]
-  (let [normalized-chars (-> input
-                             string/lower-case
-                             (string/replace #"[^a-z]" "")
-                             set)]
-    (set/subset? alphabet-set normalized-chars)))
-
+  (loop [chars input
+         found-letters #{}]
+    (if (or (empty? chars) (= 26 (count found-letters)))
+      (= 26 (count found-letters))
+      (let [current-char (Character/toLowerCase (first chars))]
+        (if (and (>= (int current-char) (int \a))
+                 (<= (int current-char) (int \z)))
+          (recur (rest chars) (conj found-letters current-char))
+          (recur (rest chars) found-letters))))))
